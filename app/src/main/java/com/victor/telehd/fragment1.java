ec8ee1374e09f11f170352343e1c5c0a173b9b32
del-ui <wambuivictor@students.must.ac.ke>
2022-04-05 06:29:46 +0300

    WhatsApp clone

--- app/src/main/java/com/victor/telehd/fragment1.java
+++ app/src/main/java/com/victor/telehd/fragment1.java
@@ -8,11 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment1#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class fragment1 extends Fragment {
 
 
